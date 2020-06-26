8e9ae5a16bbdcdef76bd282963e1242db037086c
tfe <tfe@protonmail.com>
2020-06-26 15:42:46 -0400

    Add app header component

---
+++ src/components/app-header/AppHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AppHeader.scss";
+
+function AppHeader() {
+  return (
+    <div className="app-header">
+      <span>HOYA</span>
+    </div>
+  );
+}
+
+export default AppHeader;
