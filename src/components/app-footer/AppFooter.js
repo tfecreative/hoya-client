cd823633667b758e57ff9aca12307a663dcc7d35
tfe <tfe@protonmail.com>
2020-07-02 10:43:09 -0400

    Add app footer

---
+++ src/components/app-footer/AppFooter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AppFooter.scss";
+
+function AppFooter() {
+  return (
+    <footer className="app-footer">
+      <div>© tfeCreative</div>
+      <i class="footer-icon fab fa-github" aria-hidden="true"></i>
+    </footer>
+  );
+}
+
+export default AppFooter;
