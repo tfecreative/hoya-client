d96cbeb92c01565447fd9ac5d1a78b72f131f9cb
tfe <tfe@protonmail.com>
2020-07-07 12:06:42 -0400

    Add redux

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Button from "Shared/Button/Button";
+
 import "./SignUp.scss";
 
 class SignUp extends Component {
@@ -14,29 +15,42 @@ class SignUp extends Component {
     });
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.userPostFetch(this.state);
+  handleSubmit = () => {
+    // TODO: submit sign up form
   };
 
   render() {
     return (
       <section className="sign-up">
-        <form className="form sign-up-form">
-          <div className="form-group">
-            <label className="form-label">Username</label>
-            <input type="text"></input>
-          </div>
-          <div className="form-group">
-            <label className="form-label">Password</label>
-            <input type="text"></input>
-          </div>
-          <div className="form-actions">
-            <Button type="submit" theme="primary">
-              Sign In
-            </Button>
-          </div>
-        </form>
+        <div className="sign-up-card">
+          <form className="form sign-up-form">
+            <div className="form-group">
+              <label className="form-label">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleChange}
+              ></input>
+            </div>
+            <div className="form-group">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              ></input>
+            </div>
+            <div className="form-actions">
+              <Button theme="primary" onClick={this.handleSubmit}>
+                Sign Up
+              </Button>
+            </div>
+          </form>
+        </div>
       </section>
     );
   }
